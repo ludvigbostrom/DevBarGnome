ecedc5acfac525461a75ad00a140ed8494824bd5
Ludvig Boström <ludvig.bostrom@trioptima.com>
2021-03-17 15:43:16 +0100

    lint fixes

--- devbar@trioptima/extension.js
+++ devbar@trioptima/extension.js
@@ -3,6 +3,7 @@
 
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const ExtensionUtils = imports.misc.extensionUtils;
@@ -12,23 +13,18 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const Soup = imports.gi.Soup;
 const Util = imports.misc.util;
-const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const ByteArray = imports.byteArray;
 
 let _httpSession;
 
-const DevBar = new Lang.Class({
-    Name: 'DevBar',	// Class Name
-    Extends: PanelMenu.Button,
+var DevBar = GObject.registerClass({
+},
+class DevBar extends PanelMenu.Button {
+    _init() {
+        super._init(1, `${Me.metadata.name} Indicator`, false);
 
-    _init: function () {
         this.stop = false;
-        
-
-        this.parent(1, `${Me.metadata.name} Indicator`, false);
-        // Get the GSchema source so we can lookup our settings
-
         _httpSession = new Soup.SessionAsync();
         Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
         let gschema = Gio.SettingsSchemaSource.new_from_directory(
@@ -38,7 +34,7 @@ const DevBar = new Lang.Class({
         );
 
         this.settings = new Gio.Settings({
-            settings_schema: gschema.lookup('org.gnome.shell.extensions.devbar', true)
+            settings_schema: gschema.lookup('org.gnome.shell.extensions.devbar', true),
         });
 
         // Watch the settings for changes
@@ -57,9 +53,8 @@ const DevBar = new Lang.Class({
         this.interval = this.settings.get_value('interval').unpack();
 
         // A label expanded and center aligned in the y-axis
-        this.toplabel = this.buildLabel("Dev bar");
+        this.toplabel = this.buildLabel('Dev bar');
 
-        
         // We add the box to the button
         // It will be showed in the Top Panel
         this.add_child(this.toplabel);
@@ -67,104 +62,91 @@ const DevBar = new Lang.Class({
         this.addRefreshItem();
         this.addPreferencesItem();
         this.update();
-    },
-    openPreferences: function () {
-        
+    }
 
+    openPreferences() {
         Util.spawn([
-            "gnome-shell-extension-prefs",
-            Me.uuid
+            'gnome-shell-extension-prefs',
+            Me.uuid,
         ]);
-    },
-    addPreferencesItem: function () {
-        
+    }
 
-        let item = new PopupMenu.PopupMenuItem("Preferences");
-        item.connect('activate', Lang.bind(this, this.openPreferences));
+    addPreferencesItem() {
+        let item = new PopupMenu.PopupMenuItem('Preferences');
+        item.connect('activate', this.openPreferences);
         this.menu.addMenuItem(item);
-    },
-    addRefreshItem: function () {
-        
+    }
 
-        let item = new PopupMenu.PopupMenuItem("Refresh");
+    addRefreshItem() {
+        let item = new PopupMenu.PopupMenuItem('Refresh');
 
-        item.connect('activate', Lang.bind(this, function () {
+        item.connect('activate', function () {
             this.loadWorkflowAsync(this.onWorkflowCallback);
-        }));
+        }.bind(this));
         this.menu.addMenuItem(item);
-    },
-    setUrl: function (url) {
-        
+    }
 
-        if (url == "") {
-            this.url = "";
-        }
-        else {
-            this.url = url.endsWith("/") ? url + this.username : url + "/" + this.username;
-        }
-    },
-    buildLabel: function (labelText) {
-        
+    setUrl(url) {
+        if (url === '')
+            this.url = '';
+        else
+            this.url = url.endsWith('/') ? url + this.username : `${url}/${this.username}`;
+    }
 
+    buildLabel(labelText) {
         return new St.Label({
             text: labelText,
             y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER
+            y_align: Clutter.ActorAlign.CENTER,
         });
-    },
-    update: function update() {
-        
-        if (this.stop) {
+    }
 
+    update() {
+        if (this.stop) {
             Mainloop.source_remove(this._timeout);
             this._timeout = null;
             return;
         }
 
-        if (this.url != "" && !this.menu.isOpen) {
+        if (this.url !== '' && !this.menu.isOpen)
             this.loadWorkflowAsync(this.onWorkflowCallback);
-        }
         if (this._timeout) {
             Mainloop.source_remove(this._timeout);
             this._timeout = null;
         }
-        this._timout = Mainloop.timeout_add_seconds(this.interval, Lang.bind(this, function mainloopTimeout() {
-            
+        this._timout = Mainloop.timeout_add_seconds(this.interval, function mainloopTimeout() {
             this.update();
-        }))
-    },
-    onWorkflowCallback: function (data) {
+        }.bind(this));
+    }
 
+    onWorkflowCallback(data) {
         this.updateLabel(data);
         this.updateMenu(data);
-    },
-    updateLabel: function (json) {
-        
+    }
+
+    updateLabel(json) {
         this.currentCount = 0;
-        let title = "";
+        let title = '';
         let displayObjects = json['metadata']['display'];
         let context = this;
         Object.keys(displayObjects).forEach(function (key) {
-
             let data = json['data'][key];
             let display = displayObjects[key];
             if (data && data.length > 0) {
                 context.currentCount += 1 + data.length;
-                title += display['symbol'] + data.length.toString() + "  ";
+                title += `${display['symbol'] + data.length.toString()}  `;
             }
         });
-        if (title == "") {
-            title = "✓";
-        }
+        if (title === '')
+            title = '✓';
         this.toplabel.set_text(title);
-    },
-    updateMenu: function (json) {
-        
+    }
+
+    updateMenu(json) {
         this.menu.removeAll();
         let displayObjects = json['metadata']['display'];
         let context = this;
         Object.keys(displayObjects).forEach(function (key) {
-
             let data = json['data'][key];
             let display = displayObjects[key];
             if (data && data.length > 0) {
@@ -172,54 +154,40 @@ const DevBar = new Lang.Class({
                 if (context.currentCount > 25) {
                     item = new PopupMenu.PopupSubMenuMenuItem(display['title']);
                     item.menu.actor.style = 'max-height: 300px;';
-                }
-                else {
+                } else {
                     item = new PopupMenu.PopupMenuItem(display['title']);
                 }
                 context.menu.addMenuItem(item);
                 for (let index in data) {
                     let issue = data[index];
                     let subItem = new PopupMenu.PopupMenuItem(issue['title']);
-                    subItem.connect('activate', Lang.bind(context, function () {
-                        Util.spawnCommandLine("xdg-open " + issue['url']);
+                    subItem.connect('activate', Function.prototype.bind(context, function () {
+                        Util.spawnCommandLine(`xdg-open ${issue['url']}`);
                     }));
-                    if (context.currentCount > 25) {
+                    if (context.currentCount > 25)
                         item.menu.addMenuItem(subItem);
-                        
-                    }
-                    else {
-                       
+                    else
                         context.menu.addMenuItem(subItem);
-                    }
-                    
                 }
             }
-
         });
-
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this.addRefreshItem();
         this.addPreferencesItem();
+    }
 
-    },
-    getUserName: function () {
-        
-
+    getUserName() {
         try {
-            let [result, stdout, stderr] = GLib.spawn_command_line_sync("whoami");
-            if (stdout != null) {
+            let [stdout] = GLib.spawn_command_line_sync('whoami');
+            if (stdout !== null)
                 return ByteArray.toString(stdout);
-            }
+        } catch (e) {
         }
-        catch (e) {
-            global.log(e);
-        }
-        return "NO_USER";
-    },
-    loadWorkflowAsync: function loadWorkflowAsync(callback) {
-        
+        return 'NO_USER';
+    }
 
+    loadWorkflowAsync(callback) {
         let context = this;
         let message = Soup.Message.new('GET', this.url);
 
@@ -227,33 +195,25 @@ const DevBar = new Lang.Class({
             _httpSession = new Soup.SessionAsync();
             Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
         }
-        _httpSession.queue_message(message, function soupQueue(session, message) {
-            
-
-            if (message.response_body.data) {
+        _httpSession.queue_message(message, function soupQueue(session, msg) {
+            if (msg.response_body.data) {
                 try {
-                    callback.call(context, JSON.parse(message.response_body.data));
-                }
-                catch (err) {
-                    global.log(err.message);
-                    context.toplabel.set_text("!");
-                    
+                    callback.call(context, JSON.parse(msg.response_body.data));
+                } catch (err) {
+                    context.toplabel.set_text('!');
                 }
             }
-
         });
-    },
-    _onPanelStatesChanged: function (settings, key) {
-        
+    }
 
+    _onPanelStatesChanged() {
         // Read the new settings
         let url = this.settings.get_value('url').unpack();
         this.setUrl(url);
         this.interval = this.settings.get_value('interval').unpack();
-    },
+    }
 
-    destroy: function (params) {
-        
+    destroy() {
         this.stop = true;
         if (_httpSession !== undefined)
             _httpSession.abort();
@@ -267,31 +227,26 @@ const DevBar = new Lang.Class({
         this.settings.disconnect(this._onUrlChangedId);
         this.settings.disconnect(this._onIntervalChangedId);
         this.parent();
-        
-
     }
 });
 
 let indicator = null;
 
+// eslint-disable-next-line no-unused-vars
 function init() {
-    
 }
 
 
+// eslint-disable-next-line no-unused-vars
 function enable() {
-    
-    indicator = new DevBar;
+    indicator = new DevBar();
     Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, indicator, 0, 'right');
 }
 
-
+// eslint-disable-next-line no-unused-vars
 function disable() {
-    
     if (indicator !== null) {
         indicator.destroy();
         indicator = null;
-        
-
     }
 }
--- devbar@trioptima/prefs.js
+++ devbar@trioptima/prefs.js
@@ -7,20 +7,20 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 
+// eslint-disable-next-line no-unused-vars
 function init() {
 }
 
+// eslint-disable-next-line no-unused-vars
 function buildPrefsWidget() {
-
     // Copy the same GSettings code from `extension.js`
     let gschema = Gio.SettingsSchemaSource.new_from_directory(
         Me.dir.get_child('schemas').get_path(),
         Gio.SettingsSchemaSource.get_default(),
         false
     );
-
-    this.settings = new Gio.Settings({
-        settings_schema: gschema.lookup('org.gnome.shell.extensions.devbar', true)
+    Me.settings = new Gio.Settings({
+        settings_schema: gschema.lookup('org.gnome.shell.extensions.devbar', true),
     });
 
     // Create a parent widget that we'll return from this function
@@ -28,55 +28,50 @@ function buildPrefsWidget() {
         margin: 18,
         column_spacing: 12,
         row_spacing: 12,
-        visible: true
+        visible: true,
     });
 
     // Add a simple title and add it to the prefsWidget
     let title = new Gtk.Label({
-        // As described in "Extension Translations", the following template
-        // lit
-        // prefs.js:88: warning: RegExp literal terminated too early
-        //label: `<b>${Me.metadata.name} Extension Preferences</b>`,
-        label: '<b>' + Me.metadata.name + ' Extension Preferences</b>',
+        label: `<b>${Me.metadata.name} Extension Preferences</b>`,
         halign: Gtk.Align.START,
         use_markup: true,
-        visible: true
+        visible: true,
     });
     prefsWidget.attach(title, 0, 0, 2, 1);
 
     let urlLabel = new Gtk.Label({
         label: 'Url:',
         halign: Gtk.Align.START,
-        visible: true
-    
+        visible: true,
     });
-    prefsWidget.attach(urlLabel, 0,3,1,1);
+    prefsWidget.attach(urlLabel, 0, 3, 1, 1);
 
     let inputField = new Gtk.Entry({
-        text: this.settings.get_string('url'),
+        text: Me.settings.get_string('url'),
         halign: Gtk.Align.END,
-        visible: true
+        visible: true,
     });
     prefsWidget.attach(inputField, 1, 3, 1, 1);
-    this.settings.bind('url', inputField, 'text', Gio.SettingsBindFlags.DEFAULT);
+    Me.settings.bind('url', inputField, 'text', Gio.SettingsBindFlags.DEFAULT);
 
     let intervalLabel = new Gtk.Label({
         label: 'Refresh interval (s):',
         halign: Gtk.Align.START,
-        visible: true
+        visible: true,
     });
-    prefsWidget.attach(intervalLabel, 0,4,1,1);
+    prefsWidget.attach(intervalLabel, 0, 4, 1, 1);
     let refreshInterval = new Gtk.SpinButton({
         adjustment: new Gtk.Adjustment({
             lower: 1,
             upper: 500,
-            step_increment: 1
+            step_increment: 1,
         }),
         halign: Gtk.Align.END,
-        visible: true
+        visible: true,
     });
     prefsWidget.attach(refreshInterval, 1, 4, 1, 1);
-    this.settings.bind('interval', refreshInterval, 'value', Gio.SettingsBindFlags.DEFAULT);
+    Me.settings.bind('interval', refreshInterval, 'value', Gio.SettingsBindFlags.DEFAULT);
     // Return our widget which will be added to the window
     return prefsWidget;
 }
